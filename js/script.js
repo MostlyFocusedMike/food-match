b503a898134dddc522d2e8dfe7177740c69b78ca
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2017-09-23 00:47:19 -0400

    added basic color change for buttons

--- js/script.js
+++ js/script.js
@@ -3,8 +3,44 @@
 /*globals $:false */
 
 (function () {
+    function changeLikeValue(e) {
+        var elId = e.target.id,
+        likeValClass = document.getElementById(elId).classList.item(1),
+        newLikeVal = parseInt(likeValClass.slice(-1)) + 1,
+        newLikeValClass;
+        if (newLikeVal === 4) {
+          newLikeVal = 0
+        }
+        newLikeValClass = likeValClass.slice(0, -1) + newLikeVal;
+        document.getElementById(elId).classList.remove(likeValClass);
+        document.getElementById(elId).classList.add(newLikeValClass);
+        
+    }
+    function changeIcon(e) {
+        var elId = e.target.classList.item(1);
+
+        switch(elClass) {
+            case "like-value-0":
 
 
+
+
+
+
+
+        }
+
+    } 
+
+
+    memberOpinions = document.getElementsByClassName("member-opinion");
+    for (var i=0; i <memberOpinions.length; i++) {
+        memberOpinions[i].addEventListener("click", function(e) {
+            changeLikeValue(e);
+
+        }, false);
+
+    }
 
 
 }());
