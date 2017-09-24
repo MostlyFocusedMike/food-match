006f8c1cc677febff2c7401613b60550834eb8c2
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2017-09-23 23:27:44 -0400

    button color and icon now change with each click

--- js/script.js
+++ js/script.js
@@ -3,41 +3,46 @@
 /*globals $:false */
 
 (function () {
+    'use strict';
     function changeLikeValue(e) {
         var elId = e.target.id,
         likeValClass = document.getElementById(elId).classList.item(1),
         newLikeVal = parseInt(likeValClass.slice(-1)) + 1,
-        newLikeValClass;
+        newLikeValClass, icon;
         if (newLikeVal === 4) {
           newLikeVal = 0
         }
         newLikeValClass = likeValClass.slice(0, -1) + newLikeVal;
         document.getElementById(elId).classList.remove(likeValClass);
         document.getElementById(elId).classList.add(newLikeValClass);
-        
-    }
-    function changeIcon(e) {
-        var elId = e.target.classList.item(1);
-
-        switch(elClass) {
+            
+        switch(newLikeValClass) {
             case "like-value-0":
-
-
-
-
-
-
-
+                 //selects the first child element whith tag i
+                icon = document.getElementById(elId).getElementsByTagName('i')[0];
+                icon.textContent = "help_outline"; 
+                break;
+            case "like-value-1":
+                icon = document.getElementById(elId).getElementsByTagName('i')[0];
+                icon.textContent = "thumb_up"; 
+                break;
+            case "like-value-2":
+                icon = document.getElementById(elId).getElementsByTagName('i')[0];
+                icon.textContent = "thumbs_up_down"; 
+                break;
+            case "like-value-3":
+                icon = document.getElementById(elId).getElementsByTagName('i')[0]; 
+                icon.textContent = "thumb_down"; 
+                break;
         }
+       
+    }
 
-    } 
 
-
-    memberOpinions = document.getElementsByClassName("member-opinion");
+    var memberOpinions = document.getElementsByClassName("member-opinion");
     for (var i=0; i <memberOpinions.length; i++) {
         memberOpinions[i].addEventListener("click", function(e) {
             changeLikeValue(e);
-
         }, false);
 
     }
